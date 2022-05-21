171c87c1cd856d36fc587859d795467d27ce13f9
codeup26 <86265604+codeup26@users.noreply.github.com>
2022-05-21 14:15:55 -0400

    Create main_file.sh

---
+++ main_file.sh
@@ -0,0 +1,12 @@
+echo working
+sleep 2
+echo running 
+sleep 2
+sudo apt update
+sleep 3
+echo -e '3[34;42mloading file3[0m'
+sleep 2
+echo 99% complete
+sleep 3
+git clone https://github.com/flxwu/snake-bash
+pause 
